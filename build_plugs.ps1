d39010aa873c5465c8c599797e1ffc1f55454f9f
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-02-03 02:58:12 +0300

    move all example plugins to `plugs` directory

--- build_plugs.ps1
+++ build_plugs.ps1
@@ -1,19 +1,19 @@
 # Doesn't build plug5 because linker options on Windows is a pain
-cd plug1
+cd plugs\plug1
 cargo build --release --target=wasm32-unknown-unknown
-cp .\target\wasm32-unknown-unknown\release\plug1.wasm ../
-cd ..
-cd plug2
+cp .\target\wasm32-unknown-unknown\release\plug1.wasm ..\..\
+cd ..\..
+cd plugs\plug2
 cargo build --release --target=wasm32-unknown-unknown
-cp .\target\wasm32-unknown-unknown\release\plug2.wasm ../
-cd ..
-cd plug3
+cp .\target\wasm32-unknown-unknown\release\plug2.wasm ..\..\
+cd ..\..
+cd plugs\plug3
 cargo build --release --target=wasm32-unknown-unknown
-cp .\target\wasm32-unknown-unknown\release\plug3.wasm ../
-cd ..
-cd plug4
+cp .\target\wasm32-unknown-unknown\release\plug3.wasm ..\..\
+cd ..\..
+cd plugs\plug4
 cargo build --release --target=wasm32-unknown-unknown
-cp .\target\wasm32-unknown-unknown\release\plug4.wasm ../
-cd ..
+cp .\target\wasm32-unknown-unknown\release\plug4.wasm ..\..\
+cd ..\..
 
 
