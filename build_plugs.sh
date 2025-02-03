a9c192f6271e52933eb44e643da2b1c0b8079f73
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-02-03 03:04:27 +0300

    fix `build_plugs.sh`

--- build_plugs.sh
+++ build_plugs.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 cd ./plugs/plug1
 cargo build --release --target=wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release plug1.wasm ../../
+cp ./target/wasm32-unknown-unknown/release/plug1.wasm ../../
 cd ../..
 cd ./plugs/plug2
 cargo build --release --target=wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release plug2.wasm ../../
+cp ./target/wasm32-unknown-unknown/release/plug2.wasm ../../
 cd ../..
 cd ./plugs/plug3
 cargo build --release --target=wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release plug3.wasm ../../
+cp ./target/wasm32-unknown-unknown/release/plug3.wasm ../../
 cd ../..
 cd ./plugs/plug4
 cargo build --release --target=wasm32-unknown-unknown
-cp ./target/wasm32-unknown-unknown/release plug4.wasm ../../
+cp ./target/wasm32-unknown-unknown/release/plug4.wasm ../../
 cd ../..
 cd ./plugs/plug5
 clang --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -Wl,--allow-undefined -o plug5.wasm plug5.c
