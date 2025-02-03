97ac0e1610a6f810c3cc3cbb6bb397ff33ef4778
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-02-03 21:30:48 +0300

    seperate metadata (deps) extraction into a different temporary linker and instance so the actual instance of a plugin doesnt define unknown imports as null which makes it throw an error when an unknown import is used

--- embed/src/lib.rs
+++ embed/src/lib.rs
@@ -7,7 +7,7 @@ use wasmtime::{
 pub struct Plug {
     pub module: Module,
     pub linker: Linker<()>,
-    pub instance: Instance,
+    pub instance: Option<Instance>,
 }
 
 pub struct PlugsLinker<'a>(&'a mut Linker<()>);
@@ -49,23 +49,14 @@ where
         }
     }
 
-    /// Add plug (without linking except the core library)
-    pub fn add(&mut self, file_path: &'a str, engine: &Engine) -> wasmtime::Result<()> {
-        let fp = Path::new(file_path);
-        let ext = fp.extension().unwrap();
-        let ext_len = ext.len();
-        let name = fp.file_name().unwrap().to_str().unwrap();
-        let len = name.len();
-        let name = &name[..len - ext_len - 1];
-        let module = Module::from_file(engine, file_path)?;
+    /// Extract metadata from the specified module by instantiating a temporary instance and running the
+    /// necessary reserved functions (such as `deps`) for metadata extraction.
+    pub fn extract_metadata(&mut self, engine: &Engine, module: &Module) -> Vec<String> {
         let mut linker = Linker::new(engine);
         linker.allow_shadowing(true);
-        linker.define_unknown_imports_as_default_values(&module)?;
-
-        // Link core library (optional)
-        if let Some(f) = &self.core_linker {
-            f(PlugsLinker(&mut linker))?;
-        }
+        linker
+            .define_unknown_imports_as_default_values(&module)
+            .unwrap();
 
         let instance = match linker.instantiate(&mut self.store, &module) {
             Ok(i) => i,
@@ -78,42 +69,64 @@ where
         // TODO: The plugin name could also be extracted in a similar way instead of
         // relying on the file name. The current file name approach makes the system simpler
         // but I think I will switch to a `name` export in the future.
-        // Extract dependencies (optional)
-        let deps = {
-            let mut res = Vec::new();
 
-            if let Ok(deps_fn) = instance.get_typed_func::<(), u32>(&mut self.store, "deps") {
-                let mut deps_ptr = deps_fn.call(&mut self.store, ())?;
-                let memory = instance
-                    .get_memory(&mut self.store, "memory")
-                    .expect("No 'memory' export");
-                let mut deps_buf = vec![0u8];
-                res.push(String::new());
+        // Extract dependencies (optional)
+        let mut res = Vec::new();
+        if let Ok(deps_fn) = instance.get_typed_func::<(), u32>(&mut self.store, "deps") {
+            let mut deps_ptr = deps_fn.call(&mut self.store, ()).expect(&format!(
+                "Call to `deps` function failed in module {:?}",
+                module.name()
+            ));
+            let memory = instance
+                .get_memory(&mut self.store, "memory")
+                .expect("No 'memory' export");
+            let mut deps_buf = vec![0u8];
+            res.push(String::new());
+            memory
+                .read(&mut self.store, deps_ptr as usize, &mut deps_buf)
+                .unwrap();
+            while deps_buf[0] != 0 {
+                let c = deps_buf[0] as char;
+                if c == ';' {
+                    res.push(String::new());
+                } else {
+                    res.last_mut().unwrap().push(c);
+                }
+                deps_ptr += 1;
                 memory
                     .read(&mut self.store, deps_ptr as usize, &mut deps_buf)
                     .unwrap();
-                while deps_buf[0] != 0 {
-                    let c = deps_buf[0] as char;
-                    if c == ';' {
-                        res.push(String::new());
-                    } else {
-                        res.last_mut().unwrap().push(c);
-                    }
-                    deps_ptr += 1;
-                    memory
-                        .read(&mut self.store, deps_ptr as usize, &mut deps_buf)
-                        .unwrap();
-                }
             }
-            res
-        };
+        }
+        res
+    }
+
+    /// Add plug (without linking except the core library)
+    pub fn add(&mut self, file_path: &'a str, engine: &Engine) -> wasmtime::Result<()> {
+        let fp = Path::new(file_path);
+        let ext = fp.extension().unwrap();
+        let ext_len = ext.len();
+        let name = fp.file_name().unwrap().to_str().unwrap();
+        let len = name.len();
+        let name = &name[..len - ext_len - 1];
+        let module = Module::from_file(engine, file_path)?;
+
+        let deps = self.extract_metadata(engine, &module);
+
+        let mut linker = Linker::new(engine);
+        linker.allow_shadowing(true);
+
+        // Link core library (optional)
+        if let Some(f) = &self.core_linker {
+            f(PlugsLinker(&mut linker))?;
+        }
         self.deps.insert(String::from(name), deps);
         self.items.insert(
             name,
             Plug {
                 module,
                 linker,
-                instance,
+                instance: None,
             },
         );
         self.order.push(name.to_string());
@@ -134,14 +147,22 @@ where
         for name in self.order.iter() {
             let deps = self.deps.get(name).unwrap();
             println!("\n[Plugs::link]: {name} has {deps:?} as dependencies");
-            let p = std::ptr::from_mut(self.items.get_mut(name.as_str()).unwrap());
+            let p = self.items.get_mut(name.as_str()).unwrap();
+            let p = std::ptr::from_mut(p);
             for dep in deps.iter() {
                 if let Some(p_dep) = self.items.get_mut(dep.as_str()) {
-                    let exports = p_dep
-                        .instance
-                        .exports(&mut self.store)
-                        .map(|e| (e.name().to_string(), e.into_extern()))
-                        .collect::<Vec<_>>();
+                    let exports = {
+                        if let Some(inst) = &p_dep.instance {
+                            inst
+                        } else {
+                            return Err(wasmtime::Error::msg(format!(
+                                "Dependency {dep} in plugin {name} hasn't been instantiated yet."
+                            )));
+                        }
+                    }
+                    .exports(&mut self.store)
+                    .map(|e| (e.name().to_string(), e.into_extern()))
+                    .collect::<Vec<_>>();
                     for (key, export) in exports {
                         if !["memory", "__data_end", "__heap_base", "deps", "name"] // Reserved exports
                             .contains(&key.as_str())
@@ -158,10 +179,8 @@ where
                     return Err(wasmtime::Error::msg(format!("{dep} is not a valid import")));
                 }
             }
-            if deps.len() > 0 {
-                let p = self.items.get_mut(name.as_str()).unwrap();
-                p.instance = p.linker.instantiate(&mut self.store, &p.module)?;
-            }
+            let p = self.items.get_mut(name.as_str()).unwrap();
+            p.instance = Some(p.linker.instantiate(&mut self.store, &p.module)?);
         }
         Ok(())
     }
@@ -185,7 +204,13 @@ where
     ) -> Result<TypedFunc<P, R>, wasmtime::Error> {
         // TODO: Store initial exports of plugins before linking and use that as a lookup table in this function
         if let Some(p) = self.items.get_mut(plug) {
-            p.instance.get_typed_func::<P, R>(&mut self.store, func)
+            if let Some(inst) = &p.instance {
+                inst.get_typed_func::<P, R>(&mut self.store, func)
+            } else {
+                Err(wasmtime::Error::msg(format!(
+                    "Plugin {plug} hasn't been instantiated yet."
+                )))
+            }
         } else {
             Err(wasmtime::Error::msg(format!(
                 "Couldn't find function {func} in plugin {plug}."
