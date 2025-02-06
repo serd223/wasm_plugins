f3c70c52fa87117527245c7b77869f7b1f64fe7d
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-02-06 20:41:18 +0300

    add support for building `plug5` on windows

--- build_plugs.ps1
+++ build_plugs.ps1
@@ -15,5 +15,9 @@ cd plugs\plug4
 cargo build --release --target=wasm32-unknown-unknown
 cp .\target\wasm32-unknown-unknown\release\plug4.wasm ..\..\
 cd ..\..
+cd plugs\plug5
+clang --target=wasm32 --no-standard-libraries -Wl','--export-all -Wl','--no-entry -Wl','--allow-undefined -o plug5.wasm plug5.c
+cp .\plug5.wasm ..\..\
+cd ..\..
 
 
